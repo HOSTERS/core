5b8e477be312456034930b0af326d7e397dec70a
Volkan Gezer <volkangezer@gmail.com>
2014-05-25 20:59:04 +0200

    fix some minor issues

--- apps/files_encryption/templates/invalid_private_key.php
+++ apps/files_encryption/templates/invalid_private_key.php
@@ -5,7 +5,7 @@
 		<?php p($_['message']); ?>
 		<br/>
 		<?php if($_['errorCode'] === \OCA\Encryption\Crypt::ENCRYPTION_PRIVATE_KEY_NOT_VALID_ERROR): ?>
-			<?php>p($l->t('Go directly to your ')); ?> <a href="<?php echo $location?>"><?php p($l->t('personal settings')); ?>.</a>
+			<?php>p($l->t('Go directly to your %s', array('<a href="'.$location.'">'.$l->t('personal settings').'</a>'))); ?>.
 		<?php endif; ?>
 		<br/>
 	</li>
